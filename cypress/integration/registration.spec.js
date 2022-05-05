828178313e8bd03c63d94b818e852e9fa02a2fc0
Angela Abdulina <despairrr@bk.ru>
2022-05-05 13:23:57 +0200

    full registration flow

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -2,6 +2,7 @@
 
 describe("Registration", () => {
     beforeEach(() => {
+        cy.clearCookies()
         cy.visit("/")
     })
 it("Registration flow at Civey.com", () => {
@@ -10,28 +11,36 @@ it("Registration flow at Civey.com", () => {
     {
         gender:"female",
         birthYear:"1990",
-        "country":"CH",
-        "zipCode": "7728"
+        country:"CH",
+        zipCode: "7500"
     }
 
     function signUp() {
         cy.get('[data-test="primary-navbar"]').contains('Anmelden').click()
-        cy.get('input[name="email"]').type("ang.test@gmail.com")
-        cy.get('button[type="submit"]').click()
 
-        cy.get('[class^=signup-user-module]').should('contain', 'ang.test@gmail.com')
+        cy.contains("Willkommen zurück!").should('be.visible')
+
+        cy.getByName("email").type("ang.test1@gmail.com")
+        cy.get('[type="submit"]').should('be.visible').click()
+
+        cy.get('[class^=signup-user-module]').should('contain', 'ang.test1@gmail.com')
 
         cy.get( `[for="${user.gender.toUpperCase()}"]`).click()
     
-        cy.get('[name="birth_year"]').type(user.birthYear)
-        cy.get('[name="zip"]').type(user.zipCode)
+        cy.getByName("birth_year").type(user.birthYear)
+
+        cy.getByName("zip").type(user.zipCode)
 
         cy.get('[data-test="country-select"]').click()
         cy.get('reach-portal').should('be.visible')
         cy.contains(user.country).click({force:true})
 
         
-        cy.get('[name="agreed_with_terms_of_use"]').click({force:true})
+        cy.getByName("agreed_with_terms_of_use").click({force:true})
+
+        cy.get('button[type="submit"]').click()
+
+        cy.contains('Jetzt ins Postfach schauen!').should('be.visible')
 
     }
     signUp()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getByName', (name) => {
+    return cy.get(`[name=${name}]`)
+ })
